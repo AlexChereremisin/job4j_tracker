7242667dd42baa61dc97021f32399dc5857fd592
Alexey Cheremisin <71m25@mail.ru>
2020-05-12 12:24:05 +0300

    5. Tracker - хранилище [#281882]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,24 +1,51 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
     private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
+    private int position = 0;
 
     public Item add(Item item) {
         item.setId(generateId());
-        items[size++] = item;
+        this.items[this.position++] = item;
         return item;
     }
 
     private String generateId() {
-        return String.valueOf(ids++);
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[this.position];
+        int count = 0;
+        for (int index = 0; index < this.position; index++) {
+            if (this.items[index] != null) {
+                rsl[count] = this.items[index];
+                count++;
+            }
+        }
+        return Arrays.copyOf(rsl, count);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[this.position];
+        int count = 0;
+        for (int index = 0; index < this.position; index++) {
+            if (this.items[index] != null && this.items[index].getName().equals(key)) {
+                rsl[count] = this.items[index];
+                count++;
+            }
+        }
+        return Arrays.copyOf(rsl, count);
     }
 
     public Item findById(String id) {
         Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
+        for (int index = 0; index < this.position; index++) {
+            Item item = this.items[index];
             if (item.getId().equals(id)) {
                 rsl = item;
                 break;
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -13,4 +13,67 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
+    @Test
+    public void whenAddNewItemsThenTrackerCanFindAll() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] expected = {item1, item2, item3};
+        Item[] result = tracker.findAll();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenTrackerHasNoItemWhenNoFind() {
+        Tracker tracker = new Tracker();
+        Item[] expected = new Item[0];
+        Item[] result = tracker.findAll();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenAddNewItemsThenTrackerCanFindByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test2");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] expected = {item2, item3};
+        Item[] result = tracker.findByName("test2");
+        assertThat(result, is(expected));
+    }
+
+    public void whenItemNameNoExistingThenTrackerCanNotFindByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test2");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] expected = new Item[0];
+        Item[] result = tracker.findByName("test4");
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenAddNewItemsButFindNotExistingIdWhenNull() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item expected = null;
+        Item result = tracker.findById("12345");
+        assertThat(result, is(expected));
+    }
 }

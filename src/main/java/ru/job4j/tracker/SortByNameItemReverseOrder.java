7755dc98f60561c5b0fcff575cf7e0ddb69e06ff
AlexChereremisin <71m25@mail.ru>
2020-05-25 10:48:38 +0300

    0. Сортировка(исправил код в теле метода SortByNameItemReverseOrder.compare) [#281791]

--- src/main/java/ru/job4j/tracker/SortByNameItemReverseOrder.java
+++ src/main/java/ru/job4j/tracker/SortByNameItemReverseOrder.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class SortByNameItemReverseOrder implements Comparator<Item> {
     @Override
     public int compare(Item first, Item second) {
-        return -first.getName().compareToIgnoreCase(second.getName());
+        return second.getName().compareToIgnoreCase(first.getName());
     }
 }

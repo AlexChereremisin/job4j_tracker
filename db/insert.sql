f045658ceea279bff27753bde1b53bceb8436336
Alexey Cheremisin <71m25@mail.ru>
2021-03-17 02:34:59 +0300

    1. Sql схема для проекта Tracker. [#300337]

---
+++ db/insert.sql
@@ -0,0 +1,11 @@
+insert into items(name) values ('item 1');
+insert into items(name) values ('item 2');
+insert into items(name) values ('item 3');
+insert into items(name) values ('item 4');
+insert into items(name) values ('item 5');
+insert into items(name) values ('item 6');
+insert into items(name) values ('item 7');
+insert into items(name) values ('item 8');
+insert into items(name) values ('item 9');
+insert into items(name) values ('item 10');
+insert into items(name) values ('item 11');
